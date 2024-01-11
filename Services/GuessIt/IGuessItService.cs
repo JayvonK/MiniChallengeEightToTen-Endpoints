35e47f80f6bbd858714bf5c6edac3ea78a18fd58
JayvonK <jaykhuth@gmail.com>
2024-01-11 11:02:03 -0800

    methods

--- Services/GuessIt/IGuessItService.cs
+++ Services/GuessIt/IGuessItService.cs
@@ -2,5 +2,9 @@ namespace MiniChallengeEightToTen_Endpoints.Services.GuessIt;
 
     public interface IGuessItService
     {
-        
+        string GuessItEasy(string num);
+
+        string GuessItMedium(string num);
+
+        string GuessItHard(string num);
     }
