2e5c817a59bcd096bdd713ad83e8660f01213d6e
JayvonK <jaykhuth@gmail.com>
2024-01-11 11:02:19 -0800

    interface

--- Services/GuessIt/GuessItService.cs
+++ Services/GuessIt/GuessItService.cs
@@ -1,6 +1,19 @@
 namespace MiniChallengeEightToTen_Endpoints.Services.GuessIt;
 
-    public class GuessItService
+public class GuessItService : IGuessItService
+{
+    public string GuessItEasy(string num)
     {
-        
+        throw new NotImplementedException();
     }
+
+    public string GuessItHard(string num)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GuessItMedium(string num)
+    {
+        throw new NotImplementedException();
+    }
+}
