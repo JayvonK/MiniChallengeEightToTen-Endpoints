e312b1f3dadfcc97d589ef48c8c772674af0101b
JayvonK <jaykhuth@gmail.com>
2024-01-11 11:00:57 -0800

    clean up

--- Services/GuessIt/GuessItService.cs
+++ Services/GuessIt/GuessItService.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace MiniChallengeEightToTen_Endpoints.Services.GuessIt;
 
-namespace MiniChallengeEightToTen_Endpoints.Services.GuessIt
-{
     public class GuessItService
     {
         
     }
-}
