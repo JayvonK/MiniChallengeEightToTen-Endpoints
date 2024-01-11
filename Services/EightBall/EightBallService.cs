1f05d8685de0266d173a50488693f9f9559c08a9
JayvonK <jaykhuth@gmail.com>
2024-01-11 10:50:50 -0800

    save

--- Services/EightBall/EightBallService.cs
+++ Services/EightBall/EightBallService.cs
@@ -2,8 +2,15 @@ namespace MiniChallengeEightToTen_Endpoints.Services.EightBall;
 
 public class EightBallService : IEightBallService
 {
+    public string[] answers = new string[];
+
+    public EightBallService()
+    {
+        answers = ["My reply is no", "Try again later", "Cannot predict now", "Signs Point to No", "Definitely", "Most Likely", "My sources are saying no", "You may rely on it"]
+    }
     public string AskMe(string input)
     {
-        throw new NotImplementedException();
+        Random randomGen = new Random();
+        int randomIndex = randomGen.Next(0, answers.Length);
     }
 }
