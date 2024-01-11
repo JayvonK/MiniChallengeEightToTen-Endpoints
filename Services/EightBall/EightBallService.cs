263a43f95ab0ce569a634f21503a95e47f0ea2a3
JayvonK <jaykhuth@gmail.com>
2024-01-11 10:57:59 -0800

    builder service

--- Services/EightBall/EightBallService.cs
+++ Services/EightBall/EightBallService.cs
@@ -2,15 +2,10 @@ namespace MiniChallengeEightToTen_Endpoints.Services.EightBall;
 
 public class EightBallService : IEightBallService
 {
-    public string[] answers = new string[];
-
-    public EightBallService()
-    {
-        answers = ["My reply is no", "Try again later", "Cannot predict now", "Signs Point to No", "Definitely", "Most Likely", "My sources are saying no", "You may rely on it"]
-    }
     public string AskMe(string input)
     {
-        Random randomGen = new Random();
+        string[] answers = {"My reply is no", "Try again later", "Cannot predict now", "Signs Point to No", "Definitely", "Most Likely", "My sources are saying no", "You may rely on it"};
+        Random randomGen = new();
         int randomIndex = randomGen.Next(0, answers.Length);
         return answers[randomIndex];
     }
